75d5995e215c27ee97e75248e90282a257ea90d5
llh4github <lilinhong_coding@foxmail.com>
2024-11-01 14:47:24 +0800

    refactor: ID生成器的接口名

--- ksas-web/src/main/kotlin/config/RegisterBeans.kt
+++ ksas-web/src/main/kotlin/config/RegisterBeans.kt
@@ -2,7 +2,7 @@ package io.github.llh4github.ksas.config
 
 import io.github.llh4github.ksas.commons.IdGenerator
 import io.github.llh4github.ksas.commons.IdGeneratorProperty
-import io.github.llh4github.ksas.commons.IdGeneratorTrait
+import io.github.llh4github.ksas.commons.LongIdGenerator
 import io.github.llh4github.ksas.commons.property.WebSecurityProperty
 import org.babyfish.jimmer.sql.meta.DatabaseNamingStrategy
 import org.babyfish.jimmer.sql.runtime.DefaultDatabaseNamingStrategy
@@ -24,7 +24,7 @@ class RegisterBeans {
     fun idGenerate() = IdGeneratorProperty()
 
     @Bean
-    fun idGenerator(property: IdGeneratorProperty): IdGeneratorTrait {
+    fun idGenerator(property: IdGeneratorProperty): LongIdGenerator {
         IdGenerator.configGenerator(property)
         return IdGenerator
     }
