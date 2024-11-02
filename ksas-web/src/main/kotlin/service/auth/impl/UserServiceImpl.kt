1aea9f0276c22538c4542619577075acd3991b69
llh <lilinhong_coding@foxmail.com>
2024-11-02 16:51:25 +0800

    refactor: 增加用户方法

--- ksas-web/src/main/kotlin/service/auth/impl/UserServiceImpl.kt
+++ ksas-web/src/main/kotlin/service/auth/impl/UserServiceImpl.kt
@@ -3,16 +3,19 @@ package io.github.llh4github.ksas.service.auth.impl
 import io.github.llh4github.ksas.commons.PageQueryParam
 import io.github.llh4github.ksas.commons.PageResult
 import io.github.llh4github.ksas.dbmodel.auth.User
+import io.github.llh4github.ksas.dbmodel.auth.dto.UserAddInput
 import io.github.llh4github.ksas.dbmodel.auth.dto.UserBaseView
 import io.github.llh4github.ksas.dbmodel.auth.id
 import io.github.llh4github.ksas.dbmodel.auth.username
 import io.github.llh4github.ksas.exception.UserModuleException
 import io.github.llh4github.ksas.service.BaseServiceImpl
 import io.github.llh4github.ksas.service.auth.UserService
-import org.babyfish.jimmer.Input
 import org.babyfish.jimmer.sql.kt.KSqlClient
+import org.babyfish.jimmer.sql.kt.ast.expression.count
 import org.babyfish.jimmer.sql.kt.ast.expression.eq
 import org.babyfish.jimmer.sql.kt.ast.query.specification.KSpecification
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -20,42 +23,28 @@ import org.springframework.transaction.annotation.Transactional
 class UserServiceImpl(private val sqlClient: KSqlClient) :
     BaseServiceImpl<User>(User::class, sqlClient), UserService {
 
+    @Autowired
+    private lateinit var passwordEncoder: PasswordEncoder
+
     @Transactional
-    override fun addUnique(entity: Input<User>): User {
-        val model = entity.toEntity()
+    override fun addUnique(input: UserAddInput): User {
         createQuery {
-            where(table.username eq model.username)
-            select(table.id)
+            where(table.username eq input.username)
+            select(count(table.id))
         }.fetchOne().let {
             if (it > 0) {
                 throw UserModuleException.usernameExists(
                     message = "用户名已存在",
-                    username = model.username
+                    username = input.username
                 )
             }
+        }
+
+        val model = input.toEntity {
+            password = passwordEncoder.encode(password)
         }
         val rs = insert(model)
         checkAddResult(rs)
-        return rs.modifiedEntity
-    }
-
-    @Transactional
-    override fun checkAndUpdateById(entity: Input<User>): User {
-        val model = entity.toEntity()
-        createQuery {
-            where(table.username eq model.username)
-            where(table.id eq model.id)
-            select(table.id)
-        }.fetchOne().let {
-            if (it > 0) {
-                throw UserModuleException.usernameExists(
-                    message = "用户名已存在",
-                    username = model.username
-                )
-            }
-        }
-        val rs = update(model)
-        checkUpdateDbResult(rs)
         return rs.modifiedEntity
     }
 
