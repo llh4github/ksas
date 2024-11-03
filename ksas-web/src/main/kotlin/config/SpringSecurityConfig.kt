cfa9347a3d363330fda2fec8c02730a61c95be7a
llh <lilinhong_coding@foxmail.com>
2024-11-03 10:13:40 +0800

    feat: security 过滤器

--- ksas-web/src/main/kotlin/config/SpringSecurityConfig.kt
+++ ksas-web/src/main/kotlin/config/SpringSecurityConfig.kt
@@ -1,16 +1,26 @@
 package io.github.llh4github.ksas.config
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import io.github.llh4github.ksas.commons.JsonWrapper
 import io.github.llh4github.ksas.commons.property.WebSecurityProperty
+import io.github.llh4github.ksas.filter.JwtFilter
+import io.github.llh4github.ksas.library.JwtService
 import org.springframework.context.annotation.Bean
+import org.springframework.http.MediaType
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.AuthenticationEntryPoint
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.access.AccessDeniedHandler
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 import org.springframework.stereotype.Component
 
 @Component
 class SpringSecurityConfig(
-    val property: WebSecurityProperty
+    private val property: WebSecurityProperty,
+    private val jwtService: JwtService,
+    private val objectMapper: ObjectMapper,
 ) {
     @Bean
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
@@ -18,6 +28,15 @@ class SpringSecurityConfig(
             .formLogin { it.disable() }
             .httpBasic { it.disable() }
             .logout { it.disable() }
+            .exceptionHandling {
+                it.accessDeniedHandler(jsonAccessDeniedHandler(objectMapper))
+                it.authenticationEntryPoint(jsonAuthenticationEntryPoint(objectMapper))
+            }
+            .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+            .addFilterBefore(
+                JwtFilter(property, jwtService),
+                UsernamePasswordAuthenticationFilter::class.java
+            )
 
         return http.build()
     }
@@ -26,8 +45,29 @@ class SpringSecurityConfig(
     @Suppress("SpreadOperator")
     fun webSecurityCustomizer(): WebSecurityCustomizer {
         val annoUrls = property.anonUrls.toTypedArray()
-        return WebSecurityCustomizer { web: WebSecurity ->
+        return WebSecurityCustomizer { web ->
             web.ignoring().requestMatchers(*annoUrls)
         }
+    }
+}
+
+
+internal fun jsonAccessDeniedHandler(mapper: ObjectMapper): AccessDeniedHandler {
+    val json = JsonWrapper<Void>(msg = "用户无权访问", code = "ACCESS_DENIED")
+    return AccessDeniedHandler { _, response, _ ->
+        response.contentType = MediaType.APPLICATION_JSON_VALUE
+        response.characterEncoding = "UTF-8"
+        response.status = 200
+        response.writer.write(mapper.writeValueAsString(json))
+    }
+}
+
+internal fun jsonAuthenticationEntryPoint(mapper: ObjectMapper): AuthenticationEntryPoint {
+    val json = JsonWrapper<Void>(msg = "用户无权访问", code = "ACCESS_DENIED")
+    return AuthenticationEntryPoint { _, response, _ ->
+        response.contentType = MediaType.APPLICATION_JSON_VALUE
+        response.characterEncoding = "UTF-8"
+        response.status = 200
+        response.writer.write(mapper.writeValueAsString(json))
     }
 }
