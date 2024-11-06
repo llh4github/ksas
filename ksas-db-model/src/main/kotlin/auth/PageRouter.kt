63a3baae6f0542af22f148016f96815b35a082d8
llh <lilinhong_coding@foxmail.com>
2024-11-06 22:08:31 +0800

    fix: 适配前端结构

--- ksas-db-model/src/main/kotlin/auth/PageRouter.kt
+++ ksas-db-model/src/main/kotlin/auth/PageRouter.kt
@@ -1,6 +1,7 @@
 package io.github.llh4github.ksas.dbmodel.auth
 
 import io.github.llh4github.ksas.dbmodel.BaseModel
+import io.github.llh4github.ksas.dbmodel.extra.PageRouterMeta
 import io.swagger.v3.oas.annotations.media.Schema
 import org.babyfish.jimmer.sql.*
 
@@ -29,26 +30,7 @@ interface PageRouter : BaseModel {
     @get:Schema(description = "路由重定向")
     val redirect: String?
 
-    /**
-     * 菜单排序，值越高排的越后（只针对顶级路由）
-     */
-    @get:Schema(description = "菜单排序，值越高排的越后（只针对顶级路由）")
-    val rank: Int
-
-    /**
-     * 是否在菜单中显示
-     */
-    @get:Schema(description = "是否在菜单中显示")
-    val showLink: Boolean
-
-    @get:Schema(description = "路由元信息")
-    val icon: String?
-
-    /**
-     * 菜单名称
-     */
-    @get:Schema(description = "菜单名称")
-    val title: String
+    val meta: PageRouterMeta
 
     @ManyToMany
     @JoinTable(
