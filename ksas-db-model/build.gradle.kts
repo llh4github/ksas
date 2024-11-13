81a6ba2e14095c43bfa7b2c13c2511e27b739264
llh4github <lilinhong_coding@foxmail.com>
2024-11-13 17:23:40 +0800

    build: hibernate validator 依赖

--- ksas-db-model/build.gradle.kts
+++ ksas-db-model/build.gradle.kts
@@ -7,5 +7,6 @@ dependencies {
     ksp(libs.jimmer.ksp)
     compileOnly(libs.jimmer.sql.kotlin)
     compileOnly(libs.swagger.annotations)
+    implementation(libs.validator)
     implementation(project(":ksas-commons"))
 }
