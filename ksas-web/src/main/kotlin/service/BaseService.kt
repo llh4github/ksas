c412771b134c27a3a43106ec14e7efe37b920048
llh4github <lilinhong_coding@foxmail.com>
2024-11-13 10:44:58 +0800

    refactor: 删除废弃代码

--- ksas-web/src/main/kotlin/service/BaseService.kt
+++ ksas-web/src/main/kotlin/service/BaseService.kt
@@ -3,7 +3,6 @@ package io.github.llh4github.ksas.service
 import io.github.llh4github.ksas.dbmodel.BaseModel
 import org.babyfish.jimmer.Input
 import org.babyfish.jimmer.View
-import org.babyfish.jimmer.sql.fetcher.Fetcher
 import kotlin.reflect.KClass
 
 interface BaseService<E : BaseModel> {
@@ -20,12 +19,6 @@ interface BaseService<E : BaseModel> {
 
     fun <S : View<E>> getById(staticType: KClass<S>, id: Long): S?
     fun <S : View<E>> getByIds(staticType: KClass<S>, ids: List<Long>): List<S>
-
-    @Deprecated("Use updateById instead")
-    fun getById(id: Long, fetcher: Fetcher<E>? = null): E?
-
-    @Deprecated("Use updateById instead")
-    fun getByIds(ids: List<Long>, fetcher: Fetcher<E>? = null): List<E>
 
     /**
      * 移除数据。真实删除或逻辑删除由具体配置决定。
