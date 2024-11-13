ef0f04a5e1e1ef50b7405240cf4fb2fc8b69598c
llh <lilinhong_coding@foxmail.com>
2024-11-13 20:13:57 +0800

    feat: 验证注解移到模型类上

--- ksas-db-model/src/main/kotlin/auth/Role.kt
+++ ksas-db-model/src/main/kotlin/auth/Role.kt
@@ -1,15 +1,21 @@
 package io.github.llh4github.ksas.dbmodel.auth
 
 import io.github.llh4github.ksas.dbmodel.BaseModel
+import jakarta.validation.constraints.NotBlank
 import org.babyfish.jimmer.sql.*
+import org.hibernate.validator.constraints.Length
 
 @Entity
 @Table(name = "auth_role")
 interface Role : BaseModel {
 
+    @get:Length(min = 1, max = 50, message = "title长度必须在{min}-{max}个字符之间")
+    @get:NotBlank(message = "title不能为空")
     val title: String
 
     @Key
+    @get:Length(min = 1, max = 50, message = "code长度必须在{min}-{max}个字符之间")
+    @get:NotBlank(message = "code不能为空")
     val code: String
 
     @ManyToMany(mappedBy = "roles")
@@ -30,5 +36,5 @@ interface Role : BaseModel {
         joinColumnName = "role_id",
         inverseJoinColumnName = "page_router_id"
     )
-    val pageRouters:List<PageRouter>
+    val pageRouters: List<PageRouter>
 }
