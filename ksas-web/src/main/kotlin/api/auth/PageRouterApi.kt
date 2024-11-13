0a70f7fd754ed77caf525a252ee77030b4226b00
llh4github <lilinhong_coding@foxmail.com>
2024-11-13 16:27:00 +0800

    refactor: 使用计算属性代替复合字段

--- ksas-web/src/main/kotlin/api/auth/PageRouterApi.kt
+++ ksas-web/src/main/kotlin/api/auth/PageRouterApi.kt
@@ -1,13 +1,14 @@
 package io.github.llh4github.ksas.api.auth
 
 import io.github.llh4github.ksas.commons.JsonWrapper
+import io.github.llh4github.ksas.commons.PageResult
+import io.github.llh4github.ksas.dbmodel.auth.dto.PageRouterListView
+import io.github.llh4github.ksas.dbmodel.auth.dto.PageRouterQuerySpec
 import io.github.llh4github.ksas.dbmodel.auth.dto.PageRouterTreeView
 import io.github.llh4github.ksas.service.auth.PageRouterService
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 /**
  * 页面路由API
@@ -23,5 +24,15 @@ class PageRouterApi(
     fun allRouterTree(): JsonWrapper<List<PageRouterTreeView>> {
         val rs = pageRouterService.allRouterTree()
         return JsonWrapper.ok(rs)
+    }
+
+    @Operation(summary = "分页查询")
+    @PostMapping("/page")
+    fun pageQuery(@RequestBody spec: PageRouterQuerySpec): PageResult<PageRouterListView> {
+        return pageRouterService.pageQuery(
+            PageRouterListView::class,
+            spec,
+            spec.pageParam
+        )
     }
 }
