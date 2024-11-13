6814005cac788b07623c9c44a179098af80b688f
llh4github <lilinhong_coding@foxmail.com>
2024-11-13 10:42:28 +0800

    refactor: 删除BaseRepository，减少重复代码

--- ksas-web/src/main/kotlin/service/BaseService.kt
+++ ksas-web/src/main/kotlin/service/BaseService.kt
@@ -2,6 +2,9 @@ package io.github.llh4github.ksas.service
 
 import io.github.llh4github.ksas.dbmodel.BaseModel
 import org.babyfish.jimmer.Input
+import org.babyfish.jimmer.View
+import org.babyfish.jimmer.sql.fetcher.Fetcher
+import kotlin.reflect.KClass
 
 interface BaseService<E : BaseModel> {
 
@@ -9,7 +12,26 @@ interface BaseService<E : BaseModel> {
     fun save(entity: Input<E>): E = save(entity.toEntity())
 
     fun updateById(entity: E): E
+
     fun updateById(entity: Input<E>): E = updateById(entity.toEntity())
 
     fun getById(id: Long): E?
+    fun getByIds(ids: List<Long>): List<E>
+
+    fun <S : View<E>> getById(staticType: KClass<S>, id: Long): S?
+    fun <S : View<E>> getByIds(staticType: KClass<S>, ids: List<Long>): List<S>
+
+    @Deprecated("Use updateById instead")
+    fun getById(id: Long, fetcher: Fetcher<E>? = null): E?
+
+    @Deprecated("Use updateById instead")
+    fun getByIds(ids: List<Long>, fetcher: Fetcher<E>? = null): List<E>
+
+    /**
+     * 移除数据。真实删除或逻辑删除由具体配置决定。
+     */
+    fun removeByIds(ids: List<Long>): Boolean
+
+    fun removeById(id: Long): Boolean = removeByIds(listOf(id))
+
 }
--- ksas-web/src/main/kotlin/service/BaseServiceImpl.kt
+++ ksas-web/src/main/kotlin/service/BaseServiceImpl.kt
@@ -3,35 +3,89 @@ package io.github.llh4github.ksas.service
 import io.github.llh4github.ksas.commons.PageQueryParamTrait
 import io.github.llh4github.ksas.commons.PageResult
 import io.github.llh4github.ksas.dbmodel.BaseModel
-import io.github.llh4github.ksas.dbmodel.BaseRepository
 import io.github.llh4github.ksas.exception.DbOperateException
+import org.babyfish.jimmer.View
 import org.babyfish.jimmer.sql.kt.KSqlClient
+import org.babyfish.jimmer.sql.kt.ast.KExecutable
+import org.babyfish.jimmer.sql.kt.ast.expression.eq
+import org.babyfish.jimmer.sql.kt.ast.expression.valueIn
+import org.babyfish.jimmer.sql.kt.ast.mutation.KDeleteResult
+import org.babyfish.jimmer.sql.kt.ast.mutation.KMutableDelete
+import org.babyfish.jimmer.sql.kt.ast.mutation.KMutableUpdate
 import org.babyfish.jimmer.sql.kt.ast.mutation.KSimpleSaveResult
 import org.babyfish.jimmer.sql.kt.ast.query.KConfigurableRootQuery
+import org.babyfish.jimmer.sql.kt.ast.query.KMutableRootQuery
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.transaction.annotation.Transactional
+import org.springframework.transaction.support.TransactionTemplate
 import kotlin.reflect.KClass
 
 abstract class BaseServiceImpl<E : BaseModel>(
-    override val entityType: KClass<E>,
+    private val entityType: KClass<E>,
     private val sqlClient: KSqlClient
-) : BaseRepository<E>(sqlClient), BaseService<E> {
+) : BaseService<E> {
+
+    @Autowired
+    protected lateinit var transactionTemplate: TransactionTemplate
+
+
+    protected fun <R> createQuery(
+        block: KMutableRootQuery<E>.() -> KConfigurableRootQuery<E, R>
+    ): KConfigurableRootQuery<E, R> =
+        sqlClient.createQuery(entityType, block)
+
+    protected fun createUpdate(
+        block: KMutableUpdate<E>.() -> Unit
+    ): KExecutable<Int> =
+        sqlClient.createUpdate(entityType, block)
+
+    protected fun createDelete(
+        block: KMutableDelete<E>.() -> Unit
+    ): KExecutable<Int> =
+        sqlClient.createDelete(entityType, block)
 
     @Transactional
     override fun updateById(entity: E): E {
-        val rs = update(entity)
+        val rs = sqlClient.update(entity)
         testUpdateDbResult(rs)
         return rs.modifiedEntity
     }
 
     @Transactional
     override fun save(entity: E): E {
-        val rs = insert(entity)
+        val rs = sqlClient.update(entity)
         testAddDbResult(rs)
         return rs.modifiedEntity
     }
 
+
+    override fun <S : View<E>> getById(staticType: KClass<S>, id: Long): S? {
+        return createQuery {
+            where(table.getId<Long>() eq id)
+            select(table.fetch(staticType))
+        }.fetchOneOrNull()
+    }
+
+
+    override fun <S : View<E>> getByIds(staticType: KClass<S>, ids: List<Long>): List<S> {
+        return createQuery {
+            where(table.getId<Long>() valueIn ids)
+            select(table.fetch(staticType))
+        }.execute()
+    }
+
     override fun getById(id: Long): E? {
-        return findById(id)
+        return sqlClient.findById(entityType, id)
+    }
+
+    override fun getByIds(ids: List<Long>): List<E> {
+        return sqlClient.findByIds(entityType, ids)
+    }
+
+    override fun removeByIds(ids: List<Long>): Boolean {
+        val rs = sqlClient.deleteById(entityType, ids)
+        testDeleteDbResult(rs)
+        return true
     }
 
     /**
@@ -65,10 +119,10 @@ abstract class BaseServiceImpl<E : BaseModel>(
      * @throws DbOperateException 删除数据失败
      */
     protected fun testDeleteDbResult(
-        rs: KSimpleSaveResult<*>,
-        message: String = "删除数据失败"
+        rs: KDeleteResult,
+        message: String = "没有数据被删除"
     ) {
-        if (!rs.isModified) {
+        if (rs.totalAffectedRowCount == 0) {
             throw DbOperateException.deleteFailed(message = message)
         }
     }
