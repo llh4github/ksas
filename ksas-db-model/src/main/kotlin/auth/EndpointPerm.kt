01407cb67184977d3c3725b9d9abc9512af3a85f
llh4github <lilinhong_coding@foxmail.com>
2024-11-14 10:17:43 +0800

    feat: 端点权限数据基本接口

--- ksas-db-model/src/main/kotlin/auth/EndpointPerm.kt
+++ ksas-db-model/src/main/kotlin/auth/EndpointPerm.kt
@@ -2,9 +2,13 @@ package io.github.llh4github.ksas.dbmodel.auth
 
 import io.github.llh4github.ksas.dbmodel.BaseModel
 import io.github.llh4github.ksas.dbmodel.enums.RequestMethodEnum
+import io.swagger.v3.oas.annotations.media.Schema
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Pattern
 import org.babyfish.jimmer.sql.Entity
 import org.babyfish.jimmer.sql.Key
 import org.babyfish.jimmer.sql.Table
+import org.hibernate.validator.constraints.Length
 
 
 /**
@@ -14,14 +18,27 @@ import org.babyfish.jimmer.sql.Table
 @Table(name = "auth_endpoint_perm")
 interface EndpointPerm : BaseModel {
 
+    @get:Length(min = 1, max = 50, message = "标题长度必须在{min}-{max}个字符之间")
+    @get:NotBlank(message = "标题不能为空")
+    @get:Schema(description = "标题")
     val title: String
 
+    @get:Pattern(regexp = "^/.+", message = "路径必须以'/'开头")
+    @get:Length(min = 2, max = 50, message = "路径长度必须在{min}-{max}个字符之间")
+    @get:NotBlank(message = "路径不能为空")
+    @get:Schema(description = "路径")
     val path: String
 
+    @get:Schema(description = "请求方法")
     val method: RequestMethodEnum
 
+    @get:Schema(description = "备注")
+    @get:Length(max = 150, message = "备注最多{max}个字符")
     val remark: String?
 
     @Key
+    @get:Length(min = 1, max = 50, message = "权限码长度必须在{min}-{max}个字符之间")
+    @get:NotBlank(message = "权限码不能为空")
+    @get:Schema(description = "权限码")
     val permCode: String
 }
---
+++ ksas-web/src/main/kotlin/service/auth/EndpointPermService.kt
@@ -0,0 +1,13 @@
+package io.github.llh4github.ksas.service.auth
+
+import io.github.llh4github.ksas.dbmodel.auth.EndpointPerm
+import io.github.llh4github.ksas.dbmodel.auth.dto.EndpointPermAddInput
+import io.github.llh4github.ksas.dbmodel.auth.dto.EndpointPermUpdateInput
+import io.github.llh4github.ksas.service.BaseService
+
+interface EndpointPermService : BaseService<EndpointPerm> {
+
+    fun addUnique(input: EndpointPermAddInput): EndpointPerm
+
+    fun updateUnique(input: EndpointPermUpdateInput): EndpointPerm
+}
