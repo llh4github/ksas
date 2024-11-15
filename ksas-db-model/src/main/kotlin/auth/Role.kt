315a7d892b656f661d1c51082b3dd74f64a65b9f
llh4github <lilinhong_coding@foxmail.com>
2024-11-15 11:30:30 +0800

    fix: 关联关系配置

--- ksas-db-model/src/main/kotlin/auth/Role.kt
+++ ksas-db-model/src/main/kotlin/auth/Role.kt
@@ -1,6 +1,7 @@
 package io.github.llh4github.ksas.dbmodel.auth
 
 import io.github.llh4github.ksas.dbmodel.BaseModel
+import io.swagger.v3.oas.annotations.media.Schema
 import jakarta.validation.constraints.NotBlank
 import org.babyfish.jimmer.sql.*
 import org.hibernate.validator.constraints.Length
@@ -9,13 +10,13 @@ import org.hibernate.validator.constraints.Length
 @Table(name = "auth_role")
 interface Role : BaseModel {
 
-    @get:Length(min = 1, max = 50, message = "title长度必须在{min}-{max}个字符之间")
-    @get:NotBlank(message = "title不能为空")
+    @get:Length(min = 1, max = 50, message = "角色名长度必须在{min}-{max}个字符之间")
+    @get:NotBlank(message = "角色名称不能为空")
     val title: String
 
     @Key
-    @get:Length(min = 1, max = 50, message = "code长度必须在{min}-{max}个字符之间")
-    @get:NotBlank(message = "code不能为空")
+    @get:Length(min = 1, max = 50, message = "角色码长度必须在{min}-{max}个字符之间")
+    @get:NotBlank(message = "角色码不能为空")
     val code: String
 
     @ManyToMany(mappedBy = "roles")
@@ -27,8 +28,8 @@ interface Role : BaseModel {
         joinColumnName = "role_id",
         inverseJoinColumnName = "endpoint_id"
     )
+    @Deprecated("暂只考虑角色-页面-接口权限的关联")
     val endpointPerms: List<EndpointPerm>
-
 
     @ManyToMany
     @JoinTable(
@@ -36,5 +37,6 @@ interface Role : BaseModel {
         joinColumnName = "role_id",
         inverseJoinColumnName = "page_router_id"
     )
+    @get:Schema(description = "角色关联的页面路由")
     val pageRouters: List<PageRouter>
 }
