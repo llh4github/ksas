43b519bc2e94ac0801373943831d1e2072c63503
llh <lilinhong_coding@foxmail.com>
2024-11-24 15:07:14 +0800

    feat: 级联数据接口

--- ksas-web/src/main/kotlin/service/auth/PageRouterService.kt
+++ ksas-web/src/main/kotlin/service/auth/PageRouterService.kt
@@ -2,6 +2,7 @@ package io.github.llh4github.ksas.service.auth
 
 import io.github.llh4github.ksas.dbmodel.auth.PageRouter
 import io.github.llh4github.ksas.dbmodel.auth.dto.PageRouterAddInput
+import io.github.llh4github.ksas.dbmodel.auth.dto.PageRouterCascaderView
 import io.github.llh4github.ksas.dbmodel.auth.dto.PageRouterTreeView
 import io.github.llh4github.ksas.dbmodel.auth.dto.PageRouterUpdateInput
 import io.github.llh4github.ksas.service.BaseService
@@ -15,4 +16,9 @@ interface PageRouterService : BaseService<PageRouter>, CommonOperate<PageRouter>
 
     fun addUnique(input: PageRouterAddInput): PageRouter
     fun updateUnique(input: PageRouterUpdateInput): PageRouter
+
+    /**
+     * 级联选择数据
+     */
+    fun cascader(): List<PageRouterCascaderView>
 }
