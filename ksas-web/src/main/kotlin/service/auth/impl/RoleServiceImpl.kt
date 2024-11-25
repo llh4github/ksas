d598e5c3059287f932bb62775269bdf2a54b6886
llh4github <lilinhong_coding@foxmail.com>
2024-11-25 15:39:01 +0800

    feat: 权限数据相关接口

--- ksas-web/src/main/kotlin/service/auth/impl/RoleServiceImpl.kt
+++ ksas-web/src/main/kotlin/service/auth/impl/RoleServiceImpl.kt
@@ -3,12 +3,14 @@ package io.github.llh4github.ksas.service.auth.impl
 import io.github.llh4github.ksas.dbmodel.auth.Role
 import io.github.llh4github.ksas.dbmodel.auth.code
 import io.github.llh4github.ksas.dbmodel.auth.dto.RoleAddInput
+import io.github.llh4github.ksas.dbmodel.auth.dto.RolePermissionUpdateInput
 import io.github.llh4github.ksas.dbmodel.auth.dto.RoleUpdateInput
 import io.github.llh4github.ksas.dbmodel.auth.id
 import io.github.llh4github.ksas.exception.DbCommonException
 import io.github.llh4github.ksas.service.BaseServiceImpl
 import io.github.llh4github.ksas.service.CommonOperate
 import io.github.llh4github.ksas.service.auth.RoleService
+import io.github.llh4github.ksas.service.testAddDbResult
 import org.babyfish.jimmer.kt.isLoaded
 import org.babyfish.jimmer.sql.kt.KSqlClient
 import org.babyfish.jimmer.sql.kt.ast.expression.count
@@ -52,4 +54,11 @@ class RoleServiceImpl(
         return updateUniqueData(entity, sqlClient)
     }
 
+    @Transactional
+    override fun updatePermission(input: RolePermissionUpdateInput): Boolean {
+        // 保存新的关联关系，会先删除旧的关联关系
+        val rs = sqlClient.save(input)
+        testAddDbResult(rs)
+        return rs.isModified
+    }
 }
