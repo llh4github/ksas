e12a013e472b1224d9d54580d11fdf27a9e0ad4b
llh4github <lilinhong_coding@foxmail.com>
2024-11-29 09:19:48 +0800

    feat: 文件大小校验

--- ksas-web/src/main/kotlin/service/common/impl/OssServiceImpl.kt
+++ ksas-web/src/main/kotlin/service/common/impl/OssServiceImpl.kt
@@ -1,8 +1,10 @@
 package io.github.llh4github.ksas.service.common.impl
 
 import io.github.llh4github.ksas.commons.consts.BucketNames
+import io.github.llh4github.ksas.exception.OssException
 import io.github.llh4github.ksas.library.MinioService
 import io.github.llh4github.ksas.service.common.OssService
+import org.apache.commons.io.FileUtils
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import org.springframework.web.multipart.MultipartFile
@@ -13,6 +15,9 @@ class OssServiceImpl(private val minioService: MinioService) : OssService {
     private lateinit var baseUrl: String
 
     override fun uploadAvatar(userId: Long, file: MultipartFile): String {
+        if (file.size > 2 * FileUtils.ONE_MB) {
+            throw OssException.fileSizeExceed("文件大小不能超过2MB")
+        }
         return baseUrl + minioService.uploadFile(
             BucketNames.AVATARS,
             file, mapOf("userId" to userId.toString())
@@ -20,6 +25,9 @@ class OssServiceImpl(private val minioService: MinioService) : OssService {
     }
 
     override fun uploadFile(userId: Long, file: MultipartFile): String {
+        if (file.size > 10 * FileUtils.ONE_MB) {
+            throw OssException.fileSizeExceed("文件大小不能超过10MB")
+        }
         return baseUrl + minioService.uploadFile(
             BucketNames.ATTACHMENTS,
             file, mapOf("userId" to userId.toString())
@@ -27,6 +35,9 @@ class OssServiceImpl(private val minioService: MinioService) : OssService {
     }
 
     override fun uploadTempFile(userId: Long?, file: MultipartFile): String {
+        if (file.size > 10 * FileUtils.ONE_MB) {
+            throw OssException.fileSizeExceed("文件大小不能超过10MB")
+        }
         return baseUrl + minioService.uploadFile(
             BucketNames.TEMP, file,
             userId?.let { mapOf("userId" to userId.toString()) }.orEmpty()
