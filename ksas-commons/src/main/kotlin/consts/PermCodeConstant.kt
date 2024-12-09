0a08d6d30d116fa6b9caed69cdabcea4aafe7115
llh4github <lilinhong_coding@foxmail.com>
2024-12-09 13:58:21 +0800

    feat: 权限码常量

---
+++ ksas-commons/src/main/kotlin/consts/PermCodeConstant.kt
@@ -0,0 +1,13 @@
+package io.github.llh4github.ksas.commons.consts
+
+object PermCodeConstant {
+    /**
+     * 权限码通配符
+     */
+    const val WILDCARDS = "*"
+
+    /**
+     * 超级权限码
+     */
+    const val SUPER_PERM = "*"
+}
