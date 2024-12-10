a9847fc682bb73495b27546f1e8904d66979d53a
llh4github <lilinhong_coding@foxmail.com>
2024-12-10 11:33:00 +0800

    feat: 启用方法权限验证注解

--- ksas-web/src/main/kotlin/Main.kt
+++ ksas-web/src/main/kotlin/Main.kt
@@ -2,8 +2,10 @@ package io.github.llh4github.ksas
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 
 @SpringBootApplication
+@EnableMethodSecurity(prePostEnabled = true)
 class Main
 
 @Suppress("SpreadOperator")
