79a0cf3fc65f853d5584a73c7d52db20bcb50b73
llh4github <lilinhong_coding@foxmail.com>
2024-12-10 15:04:19 +0800

    feat: 增加文档

--- ksas-web/src/main/kotlin/service/common/PermissionCheckService.kt
+++ ksas-web/src/main/kotlin/service/common/PermissionCheckService.kt
@@ -4,13 +4,20 @@ import io.github.llh4github.ksas.library.SecurityUtil
 import org.springframework.stereotype.Service
 import org.springframework.util.AntPathMatcher
 
+/**
+ * 权限检查服务
+ * 基本用法：
+ * `@PreAuthorize("@pc.hasPermission('auth:role:view:list')")`
+ */
 @Service(value = "pc")
 class PermissionCheckService {
     private val matcher = AntPathMatcher(":")
 
-    fun hasPermission(
-        permission: String
-    ): Boolean {
+    fun hasPermission(permission: String): Boolean {
         return SecurityUtil.authorities().any { matcher.match(it.authority, permission) }
+    }
+
+    fun hasAnyPermission(vararg permissions: String): Boolean {
+        return permissions.any { hasPermission(it) }
     }
 }
