7acbf4f9135aed3f9de09003c3120e0231e71fb8
llh4github <lilinhong_coding@foxmail.com>
2024-12-23 11:44:52 +0800

    refactor: 删除模块

--- ksas-commons/src/main/kotlin/consts/DatetimeConstant.kt
+++
@@ -1,8 +0,0 @@
-package io.github.llh4github.ksas.commons.consts
-
-object DatetimeConstant {
-    /**
-     * 日期时间格式: yyyy-MM-dd HH:mm:ss
-     */
-    const val DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss"
-}
--- ksas-web/src/main/kotlin/exception/OssErrorCode.kt
+++
@@ -1,9 +0,0 @@
-package io.github.llh4github.ksas.exception
-
-import org.babyfish.jimmer.error.ErrorFamily
-
-@ErrorFamily
-enum class OssErrorCode {
-
-    FILE_SIZE_EXCEED,
-}
